84304223076b8dd1f6e0b137b26acb311516b367
Cenk <muzzyathome@gmail.com>
2022-04-27 17:13:04 +0100

    Activity 4

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,8 @@ const App : React.FC = () => {
 
   const [characters, setCharacters] = useState<Array<DisneyCharacter>>([]);
 
+  const [characterFavourites, setCharacterFavourites] = useState<Array<number>>([]);
+
   const getCharacters = async (pageNumber : number) => {
     // Utilised Axios for API calls
     const apiResponse = await axios.get(`http://api.disneyapi.dev/characters?page=${pageNumber}`);
@@ -27,7 +29,9 @@ const App : React.FC = () => {
     <div className="page">
       <Header currentPage={currentPage} />
       <Navigation currentPage={currentPage} setCurrentPage={setCurrentPage} />
-      <CharacterContainer characters={characters} />
+      <CharacterContainer   characters={characters} 
+                            characterFavourites={characterFavourites} 
+                            updateFavourites={setCharacterFavourites} />
     </div>
   );
 }
--- src/components/character.tsx
+++ src/components/character.tsx
@@ -1,8 +1,13 @@
 import { DisneyCharacter } from "../disney_character"
+interface CharacterProps{
+	character: DisneyCharacter;
+	characterFavourites: Array<number>;
+	updateFavourites: (favourites: Array<number>) => void;
+}
 
 // for our props we can reuse the DisneyCharacter interface
 // - defining an anonymous type that just has one property - a DisneyCharacter
-const Character : React.FC<{ character: DisneyCharacter}> = ( { character }) => {
+const Character : React.FC<CharacterProps> = ( { character, characterFavourites, updateFavourites }) => {
 // Define a default in case the character doesn't have an image
   let imageSrc = "https://static.wikia.nocookie.net/disney/images/5/51/?";
   //"https://picsum.photos/300/200/?blur";
@@ -13,13 +18,25 @@ const Character : React.FC<{ character: DisneyCharacter}> = ( { character }) => 
       imageSrc = character.imageUrl.substring(0, character.imageUrl.indexOf('/revision'));
   }
 
+  function toggleFavouriteForCharacter(characterId : number) {
+    if(!characterFavourites.includes(characterId)) {
+        // add to favourites
+        updateFavourites([...characterFavourites, characterId]);
+    }
+    else {
+      // remove from favourites
+      const updatedFavourites = characterFavourites.filter((id) => id !== characterId);
+      updateFavourites(updatedFavourites);
+    }
+  }
+
   return (
     <article className="character-item">
 
       <h2>{character.name}</h2>
 
-      <div className="character-item__actions">
-        Add to Favourites
+      <div className="character-item__actions" onClick={() => toggleFavouriteForCharacter(character._id)}>
+        {!characterFavourites.includes(character._id) ? "Add to Favourites" : "Favourited"}
       </div>
 
       <img className="character-item__img" src={imageSrc} alt={character.name} />
